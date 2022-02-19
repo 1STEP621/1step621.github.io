8b9f10d8065fc60c483e0998d625154c02244213
1STEP621 <86859447+1STEP621@users.noreply.github.com>
2022-02-19 10:20:32 +0900

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -6,7 +6,7 @@ $(function() {
   })
   .done(function(contents){
     $("head").prepend(contents);
-  })
+  });
   $("#header").load("https://1step621.github.io/header.html");
   $("#footer").load("https://1step621.github.io/footer.html");
 });
