4b4d6e2257901ea808abf3aab2199815766b195c
1STEP621 <86859447+1STEP621@users.noreply.github.com>
2022-02-24 19:28:14 +0900

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -4,20 +4,20 @@ $(function(){
     $("#loading").css("display","block");
   })
   .done(function(){
-    $.ajax({
-      type: "GET",
-      url: "https://1step621.github.io/head.html",
-      dataType: "html"
-    })
-    .done(function(contents){
-      $.when(function(){
-        $("head").prepend(contents);
-        $("#header").load("https://1step621.github.io/header.html");
-        $("#footer").load("https://1step621.github.io/footer.html");
+    $.when(function(){
+      $.ajax({
+        type: "GET",
+        url: "https://1step621.github.io/head.html",
+        dataType: "html"
       })
-      .done(function(){
-        $("#loading").css("display","none");
+      .done(function(contents){
+        $("head").prepend(contents);
       });
+      $("#header").load("https://1step621.github.io/header.html");
+      $("#footer").load("https://1step621.github.io/footer.html");
     })
+    .done(function(){
+      $("#loading").css("display","none");
+    });
   });
 });
