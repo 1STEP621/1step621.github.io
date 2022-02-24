8daa3987d4990a1f2ad1721392ea43489643de12
1STEP621 <86859447+1STEP621@users.noreply.github.com>
2022-02-24 20:16:15 +0900

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,19 +1,19 @@
 $(function(){
-  $("body").prepend('<div id="loading" style="background-color: var(--white); width: 100%; height: 100vh; position: fixed;"></div>');
-  $("#loading").css("display","block");
   $.fileLoad();
   $("#loading").css("display","none");
 });
 
 $(function fileLoad(){
-    $.ajax({
-      type: "GET",
-       url: "https://1step621.github.io/head.html",
-       dataType: "html"
-    })
-    .done(function(contents){
-      $("head").prepend(contents);
-    });
-    $("#header").load("https://1step621.github.io/header.html");
-    $("#footer").load("https://1step621.github.io/footer.html");
+  $("body").prepend('<div id="loading" style="background-color: var(--white); width: 100%; height: 100vh; position: fixed;"></div>');
+  $("#loading").css("display","block");
+  $.ajax({
+    type: "GET",
+    url: "https://1step621.github.io/head.html",
+    dataType: "html"
   })
+  .done(function(contents){
+    $("head").prepend(contents);
+  });
+  $("#header").load("https://1step621.github.io/header.html");
+  $("#footer").load("https://1step621.github.io/footer.html");
+});
