1342b82d18ad7874b3aa56fd9e53c61509f3dced
1STEP621 <86859447+1STEP621@users.noreply.github.com>
2022-02-24 18:00:00 +0900

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -6,7 +6,7 @@ $(function(){
     dataType: "html"
   })
   .done(function(contents){
-    .when(function(){
+    $.when(function(){
       $("head").prepend(contents);
       $("#header").load("https://1step621.github.io/header.html");
       $("#footer").load("https://1step621.github.io/footer.html");
