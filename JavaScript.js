17294b9e1f68deb0e2d73567adcfb2412af6a2c7
1STEP621 <86859447+1STEP621@users.noreply.github.com>
2022-02-25 17:12:39 +0900

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,6 @@
-var loadFile = function() {
+function loadFile() {
+  $("body").prepend('<div id="loading" style="background-color: var(--white); width: 100%; height: 100vh; position: fixed;"></div>');
+  $("#loading").css("display", "block");
   $.ajax({
       type: "GET",
       url: "https://1step621.github.io/head.html",
@@ -9,20 +11,12 @@ var loadFile = function() {
     });
   $("#header").load("https://1step621.github.io/header.html");
   $("#footer").load("https://1step621.github.io/footer.html");
-  console.log("test")
-};
-
-var loadStart = function() {
-  $("body").prepend('<div id="loading" style="background-color: var(--white); width: 100%; height: 100vh; position: fixed;"></div>');
-  $("#loading").css("display", "block");
-};
-
-var loadEnd = function() {
+  return loadEnd();
+}
+.then(function loadEnd() {
   $("#loading").css("display", "none");
-};
+});
 
 $(function() {
-  loadStart();
-  loadFile()
-  .then(loadEnd);
+  loadFile();
 });
