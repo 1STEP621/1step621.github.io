17535d3fb601cfc385eb1ffe49fde97c1ce5af1c
1STEP621 <86859447+1STEP621@users.noreply.github.com>
2022-04-09 20:37:57 +0900

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -2,6 +2,7 @@ var loadFile = function() {
   var content = 0;
   $("#header").load("https://1step621.github.io/header.html");
   $("#footer").load("https://1step621.github.io/footer.html");
+  $("#footer").prepend("<div class="loading" style="width: 100%; height: 100vh; background-color: #ffffff; position: fixed; z-index: 1000;">");
   console.log("load");
   $.ajax({
     type: "GET",
