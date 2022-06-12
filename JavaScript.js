71e969b48d2928ea53e69f4cca6d74067ba9a736
1STEP621 <86859447+1STEP621@users.noreply.github.com>
2022-06-12 16:29:14 +0900

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -21,6 +21,24 @@ var loadFile = function() {
     });
 };
 
+var setOgp = function() {
+  var title = $("title").text();
+  var url = $(location).attr("href");
+  var desc = $(".wrapper").text().slice(0, 80);
+  var head = $(head).text();
+  if (head.match("(?!/og:type/)") != null) {
+    $("head").append("<meta property=\"og:type\" content=\"artile\">");
+  }
+  if (head.match("(?!/og:description/)") != null) {
+    $("head").append("<meta property=\"og:description\" content=\"" + desc + "...\">");
+  }
+  $("head").append("<meta property=\"og:title\" content=\"" + title + "\">");
+  $("head").append("<meta property=\"og:url\" content=\"" + url + "\">");
+  $("head").append("<meta property=\"og:site_name\" content=\"1step621.github.io\">");
+  $("head").append("<meta property=\"og:image\" content=\"https://1step621.github.io/assets/1step621_icon.png\">");
+};
+
 $(function() {
   loadFile();
+  setOgp();
 });
