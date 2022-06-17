fe6abc5cc63caddb0f6fb22ba1c46899f765c9b7
1STEP621 <86859447+1STEP621@users.noreply.github.com>
2022-06-17 20:59:30 +0900

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -29,20 +29,18 @@ const setForSeo = function() {
 };
 
 const addDiaryBox = function() {
-  let titles, dates, length;
-  let title, date, content;
+  let meta, title, date;
   $.ajax({
       type: "GET",
       url: "https://1step621.github.io/diary-data/index.html",
       dataType: "html"
     })
     .done(function(result) {
-      titles = $(result).filter(".title");
-      dates = $(result).filter(".date");
-      length = $(result).filter(".title").length;
-      for (i = 0; i < length; i++) {
-        title = titles[i].outerText;
-        date = dates[i].outerText;
+      meta = $(result).filter("meta");
+      length = meta.length;
+      for (i = 0; i <= length; i++) {
+        title = $(meta[i]).data("title");
+        date = $(meta[i]).data("date");
         content = '<a href=article?id=' + date + ' class="block-link"><div class="diary-box"><h1>' + title + '</h1><time>' + date + '</time></div></a>';
         $(".test").append(content);
       }
@@ -56,7 +54,7 @@ $(function() {
   if (url.match(/^.*diary$/) != null) {
     addDiaryBox();
   }
-  if (pathName == article) {
+  if (pathName == "article") {
     /*addDiary();*/
   } else {
     setForSeo();
