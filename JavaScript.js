b150e78e33122f426d8b8ad83ea3ecb8c88d868c
1STEP621 <86859447+1STEP621@users.noreply.github.com>
2022-06-17 17:48:30 +0900

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -29,13 +29,23 @@ var setForSeo = function() {
 };
 
 var addDiary = function() {
+  var titles, dates, length;
+  var title, date, content;
   $.ajax({
       type: "GET",
-      url: "https://scratch.mit.edu/site-api/comments/gallery/31762993/",
+      url: "https://1step621.github.io/diary-data/index.html",
       dataType: "html"
     })
     .done(function(result) {
-      console.log(result);
+      titles = $(result).filter(".title");
+      dates = $(result).filter(".date");
+      length = $(result).filter(".title").length;
+      for (i = 0; i < length; i++) {
+        title = titles[i].outerText;
+        date = dates[i].outerText;
+        content = '<a href=article?id=' + date + ' class="block-link"><div class="diary-box"><h1>' + title + '</h1><time>' + date + '</time></div></a>';
+        $(".test").append(content);
+      }
     });
 };
 
