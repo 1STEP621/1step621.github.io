ce6e8c91236b63bf5db6f4b8062704e0baa06aa3
1STEP621 <86859447+1STEP621@users.noreply.github.com>
2022-07-05 22:29:21 +0900

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -23,15 +23,15 @@ const loadFile = function() {
 
 const setTitle = function() {
   let title = $('meta[property="og:title"]').attr("content");
-  $("head").append("<title>"+title+"</title>");
+  $("head").append("<title>" + title + "</title>");
 };
 
 const setDesc = function() {
   let desc = $('meta[property="og:description"]').attr("content");
-  $("head").append('<meta name="description" content="'+desc+'">');
+  $("head").append('<meta name="description" content="' + desc + '">');
 };
 
-const addDiaryBox = function() {
+const addDiaryList = function() {
   let meta, title, date;
   $.ajax({
       type: "GET",
@@ -87,10 +87,55 @@ const addDiary = function() {
       endNum = i - startNum - 1;
       thisContent = thisContent.not("meta");
 
-      $(".wrapper").html("<h1>"+thisTitle+"</h1>");
+      $(".wrapper").html("<h1>" + thisTitle + "</h1>");
       $(".wrapper").append(thisContent);
-      $(".wrapper").append("<time>"+thisDate+"</time>");
-      $("head").append("<title>"+thisTitle+" | 1STEP621のホームページ</title>");
+      $(".wrapper").append("<time>" + thisDate + "</time>");
+      $("head").append("<title>" + thisTitle + " | 1STEP621のホームページ</title>");
+    });
+};
+
+const addProject = function() {
+  let data, id, thisMeta, thisTitle, thisId, thisQuery, startNum, endNum, thisContent;
+  let i = 0;
+  let isFind = true;
+  $.ajax({
+      type: "GET",
+      url: "https://1step621.github.io/projects-data/index.html",
+      dataType: "html"
+    })
+    .done(function(result) {
+      data = $(result);
+      id = $(location).attr("search").match(/\d.*/);
+      i = 0;
+      while (isFind) {
+        isFind = $(data[i]).data("id") != id;
+        i++;
+        if (i > data.length) {
+          window.location.href = "https://1step621.github.io/404";
+          return;
+        }
+      }
+      startNum = i - 1;
+      thisMeta = data[startNum];
+      thisTitle = $(data[startNum]).data("title");
+      thisId = $(data[startNum]).data("id");
+      thisQuery = $(data[startNum]).data("query");
+
+      isFind = true;
+      i = startNum;
+      thisContent = $();
+      while (isFind) {
+        isFind = $(data[i]).attr("name") != "project-end";
+        thisContent = thisContent.add($(data[i]));
+        i++;
+      }
+      endNum = i - startNum - 1;
+      thisContent = thisContent.not("meta");
+
+      $("#scratch-project iframe").attr("src", "https://turbowarp.org/" + thisId + "/embed" + thisQuery);
+      $(".view-on-scratch").attr("src", "https://scratch.mit.edu/projects/" + thisId);
+      $(".caption").append(thisContent);
+      $("head").append("<title>" + thisTitle + "</title>")
     });
 };
 
@@ -98,10 +143,12 @@ $(function() {
   loadFile();
   const pathName = $(location).attr("pathname");
   if (pathName == "/diary/") {
-    addDiaryBox();
+    addDiaryList();
   }
   if (pathName == "/diary/article") {
     addDiary();
+  } else if (pathName == "/projects/work") {
+    addProject()
   } else {
     setTitle();
   }
